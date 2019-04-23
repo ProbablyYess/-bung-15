3a89fd5f340fbd579b1a66612ac20104637ef6b8
Hussein A <49880905+ProbablyYess@users.noreply.github.com>
2019-04-23 23:03:50 +0200

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Palindrom{
+    
+    public static boolean iterativ (String wort){
+        int vorn=0;
+        int hinten=wort.length()-1;
+        while (vorn < hinten){
+            if (wort.charAt(vorn) == wort.charAt(hinten)){
+                vorn++;
+                hinten--;
+            }
+            else return false;
+        }
+        return true;
+    }
+    
+    public static boolean rekursiv (String wort){
+        return rek(wort,0,wort.length()-1);
+    }
+    
+    private static boolean rek (String wort, int vorn, int hinten){
+        if (vorn >= hinten){
+            return true;
+        }
+        if (wort.charAt(vorn) == wort.charAt(hinten)){
+                return rek(wort, vorn+1, hinten-1);
+            }
+        else return false;
+    }
+}
